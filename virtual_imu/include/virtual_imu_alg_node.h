a7966358a0435118886325c8ada4968bf44b7a56
Iván del Pino <idelpino@iri.upc.edu>
2019-09-27 17:15:54 +0200

    included file data dumpong for offline imu calibration

--- virtual_imu/include/virtual_imu_alg_node.h
+++ virtual_imu/include/virtual_imu_alg_node.h
@@ -35,6 +35,7 @@
 #include "virtual_imu_alg.h"
 #include "sensor_msgs/Imu.h"
 #include "geometry_msgs/TwistWithCovarianceStamped.h"
+#include <fstream>
 
 // [publisher subscriber headers]
 
@@ -50,6 +51,15 @@
 class VirtualImuAlgNode : public algorithm_base::IriBaseAlgorithm<VirtualImuAlgorithm>
 {
 private:
+
+  std::ofstream acc_results_file_;
+  std::string acc_data_ready_to_be_written_to_file_;
+
+  std::ofstream gyro_results_file_;
+  std::string gyro_data_ready_to_be_written_to_file_;
+
+  bool flag_first_time_stamp_received_;
+  double first_timestamp_;
 
   sensor_msgs::Imu originl_imu_msg_;
 
--- virtual_imu/src/virtual_imu_alg_node.cpp
+++ virtual_imu/src/virtual_imu_alg_node.cpp
@@ -4,7 +4,7 @@ VirtualImuAlgNode::VirtualImuAlgNode(void) :
     algorithm_base::IriBaseAlgorithm<VirtualImuAlgorithm>()
 {
   //init class attributes if necessary
-  this->loop_rate_ = 20; //in [Hz]
+  this->loop_rate_ = 100; //in [Hz]
   this->originl_imu_msg_.angular_velocity.x = 0.0;
   this->originl_imu_msg_.angular_velocity.y = 0.0;
   this->originl_imu_msg_.angular_velocity.z = 0.0;
@@ -25,11 +25,32 @@ VirtualImuAlgNode::VirtualImuAlgNode(void) :
   // [init action servers]
 
   // [init action clients]
+
+  std::cout << "Creating output files " << std::endl;
+  std::string acc_filename = "/home/idelpino/Documents/imu_acc.mat";
+  acc_results_file_.open(acc_filename.c_str(), std::ofstream::trunc);
+
+  std::string gyro_filename = "/home/idelpino/Documents/imu_gyro.mat";
+  gyro_results_file_.open(gyro_filename.c_str(), std::ofstream::trunc);
+
+  std::cout << "Output files created!" << std::endl;
+
+  flag_first_time_stamp_received_ = false;
+  first_timestamp_ = 0.0;
+
 }
 
 VirtualImuAlgNode::~VirtualImuAlgNode(void)
 {
   // [free dynamic memory]
+  acc_results_file_  << acc_data_ready_to_be_written_to_file_;
+  gyro_results_file_ << gyro_data_ready_to_be_written_to_file_;
+
+  std::cout << acc_data_ready_to_be_written_to_file_;
+
+  std::cout << "Closing output files " << std::endl;
+  acc_results_file_.close();
+  gyro_results_file_.close();
 }
 
 void VirtualImuAlgNode::mainNodeThread(void)
@@ -52,6 +73,38 @@ void VirtualImuAlgNode::cb_imuData(const sensor_msgs::Imu& Imu_msg)
   this->originl_imu_msg_.angular_velocity.x = Imu_msg.angular_velocity.x;
   this->originl_imu_msg_.angular_velocity.y = Imu_msg.angular_velocity.y;
   this->originl_imu_msg_.angular_velocity.z = Imu_msg.angular_velocity.z;
+
+  if(!flag_first_time_stamp_received_)
+  {
+    flag_first_time_stamp_received_ = true;
+    first_timestamp_ = Imu_msg.header.stamp.sec + (Imu_msg.header.stamp.nsec * 1e-9);
+  }
+
+  double current_timestamp = Imu_msg.header.stamp.sec + (Imu_msg.header.stamp.nsec * 1e-9);
+
+  std::setiosflags(std::ios::fixed);
+  std::setprecision(11);
+  std::ostringstream s_gyro;
+  std::ostringstream s_acc;
+
+  s_gyro << "   " << current_timestamp - first_timestamp_
+         << "   " << Imu_msg.angular_velocity.x
+         << "   " << Imu_msg.angular_velocity.y
+         << "   " << Imu_msg.angular_velocity.z
+         << std::endl;
+
+  gyro_data_ready_to_be_written_to_file_ += s_gyro.str();
+
+  s_acc  << "   " << current_timestamp - first_timestamp_
+         << "   " << Imu_msg.linear_acceleration.x
+         << "   " << Imu_msg.linear_acceleration.y
+         << "   " << Imu_msg.linear_acceleration.z
+         << std::endl;
+
+  acc_data_ready_to_be_written_to_file_ += s_acc.str();
+
+  //std::cout << s_acc.str();
+
   this->alg_.unlock();
 }
 
