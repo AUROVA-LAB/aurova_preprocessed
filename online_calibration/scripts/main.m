b4acfc1be96abb65b55634b6a9d923b197507a3d
mice85 <miguelangel.munoz@ua.es>
2019-11-07 17:22:34 +0100

    ICP implemented for matching

--- online_calibration/scripts/main.m
+++ online_calibration/scripts/main.m
@@ -10,10 +10,10 @@ W_SOBEL = 1.0;
 RADIUS = 2;
 
 %************** read images ***************%
-sobel_filename_base = 'images/input/tr_01/sobel';
-discnt_filename_base = 'images/input/tr_01/discnt';
-sobel_filename_base_out = 'images/output/tr_01/plot';
-discnt_filename_base_out = 'images/output/tr_01/plot';
+sobel_filename_base = 'images/input/tr_01_dt_01/sobel';
+discnt_filename_base = 'images/input/tr_01_dt_01/discnt';
+sobel_filename_base_out = 'images/output/tr_01_dt_01/plot';
+discnt_filename_base_out = 'images/output/tr_01_dt_01/plot';
 
 for index = INI_IMAGES:STEP_IMAGES:NUM_IMAGES
 sobel_filename = strcat(sobel_filename_base, num2str(index,'%d'));
@@ -43,6 +43,8 @@ entropy_color_map = ind2rgb(uint8(entropy_map_plot*256), jet(256));
 entropy_map_plot_g = imgaussfilt(entropy_map_plot, 6.0) .* entropy_map_mask;
 maximums = imregionalmax(entropy_map_plot_g);
 [kp_y, kp_x] = find(maximums > 0); % keypoints
+% maximums = max(max(entropy_map_plot_g));
+% [kp_y, kp_x] = find(entropy_map_plot_g == maximums); % keypoints
 clear k;
 k(1:length(kp_x), 1) = RADIUS;
 kp_x_plt = kp_x + ROI_WIDTH/2;
@@ -50,6 +52,46 @@ kp_y_plt = kp_y + ROI_HEIGHT/2;
 entropy_map_plot_g = insertShape(entropy_map_plot_g, 'circle', [kp_x, kp_y, k], 'LineWidth', 2, 'Color', 'green');
 discnt_plot = insertShape(discnt_image, 'circle', [kp_x_plt, kp_y_plt, k], 'LineWidth', 2, 'Color', 'green');
 sobel_plot = insertShape(sobel_image, 'circle', [kp_x_plt, kp_y_plt, k], 'LineWidth', 2, 'Color', 'green');
+
+%*****************************************%
+% THRESHOLD_TM = 20;
+% THRESHOLD = 0;
+% REDUCTION = 20;
+% len = length(kp_x);
+% kp_u = zeros(len, 1);
+% kp_v = zeros(len, 1);
+% for n = 1:len
+%     template = discnt_image(kp_y(n):kp_y(n)+ROI_HEIGHT-1, kp_x(n):kp_x(n)+ROI_WIDTH-1);
+%     [pt_y_tm, pt_x_tm] = find(template > THRESHOLD_TM); % cloud template
+%     clear pt_z_tm;
+%     pt_z_tm(1:length(pt_x_tm), 1) = double(0);
+%     for i = 1:length(pt_x_tm)
+%         pt_z_tm(i, 1) = (template(pt_y_tm(i, 1), pt_x_tm(i, 1)) - THRESHOLD_TM) / REDUCTION;
+%     end
+%     pt_x_tm = pt_x_tm + kp_x(n);
+%     pt_y_tm = pt_y_tm + kp_y(n);
+%     pt_xyz_tm = [pt_x_tm'; pt_y_tm'; pt_z_tm']';
+%     pt_cloud_tm = pointCloud(pt_xyz_tm);
+%     [pt_y, pt_x] = find(sobel_image(:, :, 1) > THRESHOLD); % cloud image
+%     clear pt_z;
+%     pt_z(1:length(pt_x), 1) = double(0);
+%      for i = 1:length(pt_x)
+%         pt_z(i, 1) = (sobel_image(pt_y(i, 1), pt_x(i, 1)) - THRESHOLD) / REDUCTION;
+%     end
+%     pt_xyz = [pt_x'; pt_y'; pt_z']';
+%     pt_cloud = pointCloud(pt_xyz);
+%     tform = pcregistericp(pt_cloud_tm, pt_cloud); % register ICP
+%     trvec = tform2trvec(tform.T');
+%     kp_u = kp_x(n) + trvec(1);
+%     kp_v = kp_y(n) + trvec(2);
+%     kp_u_plt = kp_u + ROI_WIDTH/2;
+%     kp_v_plt = kp_v + ROI_HEIGHT/2;
+%     kp_x_plt = kp_x(n) + ROI_WIDTH/2;
+%     kp_y_plt = kp_y(n) + ROI_HEIGHT/2;
+%     sobel_plot = insertShape(sobel_plot, 'line', [kp_u_plt kp_v_plt kp_x_plt kp_y_plt], 'LineWidth', 1, 'Color', 'yellow');
+%     sobel_plot = insertShape(sobel_plot, 'circle', [kp_u_plt kp_v_plt RADIUS], 'LineWidth', 2, 'Color', 'red');
+%     %match = sobel_image(kp_v:kp_v+ROI_HEIGHT-1, kp_u:kp_u+ROI_WIDTH-1);
+% end
 
 %************ KL divergence **************%
 len = length(kp_x);
