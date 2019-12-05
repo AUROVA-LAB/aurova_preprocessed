b01b1a97a45ba5443b83d864e6f40e268a0f2771
mice85 <37371068+mice85@users.noreply.github.com>
2019-12-05 19:03:33 +0100

    code consolidation, execution time optimization, and gpu proofs

--- online_calibration/scripts/main.m
+++ online_calibration/scripts/main.m
@@ -1,35 +1,13 @@
 clear, clc, close all
 
 %****************** global variables ******************%
+is_m_data = true;
 index = 450;
 window_size = 300;
-scan_filename_base = 'images/input/raw_data_01/scan';
-tf_filename_base = 'images/input/raw_data_01/tf';
 
-%******** transform (to map) and acumulation *********%
-scans_sec = {};
-step = -1;
-ini = index + window_size/2;
-endl = index - window_size/2;
-for n = ini:step:endl
-    
-    scan_filename = strcat(scan_filename_base, num2str(n,'%d.pcd'));
-    tf_filename = strcat(tf_filename_base, num2str(n,'%d.csv'));
-    scan = pcread(scan_filename);
-    xyz_rpy = csvread(tf_filename);
-    tform_lidar2map = getTfAffineMatrix(xyz_rpy, 1);
-    if n == index
-        tform_map2camera = getTfAffineMatrix(xyz_rpy, 2);
-    end
-
-    scan = helperProcessPointCloud(scan);
-    scan = pctransform(scan, tform_lidar2map);
-    scans_sec = [scans_sec; {scan}];
-    
-end
-% cloud = helperProcessPointCloud(cloud);
-
-
+%************* read and store raw data ****************%
+[scans_lidarframe, scans_mapframe, tfs_lidar2map, tfs_map2camera] = ...
+    readData(is_m_data, index, window_size);
 
 %********** project 3D points into 2D pixel plane ********%;
 camera_params = getCameraParams();
@@ -37,19 +15,21 @@ xy = camera_params.ImageSize;
 image_cloud(1:xy(2), 1:xy(1)) = uint8(0);
 sigma = 10;
 base = 255;
-m = length(scans_sec);
+m = length(scans_mapframe);
+id_secuence = window_size / 2;
+tform_map2camera = tfs_map2camera{id_secuence};
 for j = 1:m
-    scans_sec{j} = pctransform(scans_sec{j}, tform_map2camera);
-    camframe_pts = scans_sec{j}.Location;
-    camframe_pts = camframe_pts(camframe_pts(:, 3) > 0, :);
-    image_points = worldToImageSimple(camera_params, camframe_pts);
-
-    n = length(camframe_pts(:, 1));
+    scan_cameraframe = pctransform(scans_mapframe{j}, tform_map2camera);
+    points_camframe = scan_cameraframe.Location;
+    points_camframe = points_camframe(points_camframe(:, 3) > 0, :);
+    
+    image_points = worldToImageSimple(camera_params, points_camframe);
+    n = length(points_camframe(:, 1));
     for i = 1:n
         u = round(image_points(i, 1));
         v = round(image_points(i, 2));
         if (v >= 1 && v <= xy(2)) && (u >= 1 && u <= xy(1))
-            dist_value = camframe_pts(i, 3);
+            dist_value = points_camframe(i, 3);
             image_cloud(v, u) = mapDistanceToUint(dist_value, sigma, base);
         end
     end
---
+++ online_calibration/scripts/readData.m
@@ -0,0 +1,44 @@
+function [scans_lidarframe, scans_mapframe, tfs_lidar2map, tfs_map2camera] = ...
+    readData(is_m_data, index, window_size)
+
+if is_m_data
+    scans_lidarframe = load('mat_data/scans_lidarframe.mat', 'scans_lidarframe');
+    scans_mapframe = load('mat_data/scans_mapframe.mat', 'scans_mapframe');
+    tfs_lidar2map = load('mat_data/tfs_lidar2map.mat', 'tfs_lidar2map');
+    tfs_map2camera = load('mat_data/tfs_map2camera.mat', 'tfs_map2camera');
+    scans_lidarframe = scans_lidarframe.scans_lidarframe;
+    scans_mapframe = scans_mapframe.scans_mapframe;
+    tfs_lidar2map = tfs_lidar2map.tfs_lidar2map;
+    tfs_map2camera = tfs_map2camera.tfs_map2camera;
+else
+    scan_filename_base = 'raw_data/input/raw_data_01/scan';
+    tf_filename_base = 'raw_data/input/raw_data_01/tf';
+    step = -1;
+    ini = index + window_size/2;
+    endl = index - window_size/2;
+    scans_lidarframe = {};
+    scans_mapframe = {};
+    tfs_lidar2map = {};
+    tfs_map2camera = {};
+    for n = ini:step:endl
+        scan_filename = strcat(scan_filename_base, num2str(n,'%d.pcd'));
+        tf_filename = strcat(tf_filename_base, num2str(n,'%d.csv'));
+        scan = pcread(scan_filename);
+        xyz_rpy = csvread(tf_filename);
+        tform_lidar2map = getTfAffineMatrix(xyz_rpy, 1);
+        tform_map2camera = getTfAffineMatrix(xyz_rpy, 2);
+        scan_mapframe = pctransform(scan, tform_lidar2map);
+        tfs_lidar2map = [tfs_lidar2map; {tform_lidar2map}];
+        tfs_map2camera = [tfs_map2camera; {tform_map2camera}];
+        scans_lidarframe = [scans_lidarframe; {scan}];
+        scans_mapframe = [scans_mapframe; {scan_mapframe}];
+        
+    end
+    save('mat_data/scans_lidarframe.mat', 'scans_lidarframe');
+    save('mat_data/scans_mapframe.mat', 'scans_mapframe');
+    save('mat_data/tfs_lidar2map.mat', 'tfs_lidar2map');
+    save('mat_data/tfs_map2camera.mat', 'tfs_map2camera');
+end
+
+end
+
