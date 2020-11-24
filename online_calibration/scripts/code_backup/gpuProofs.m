7e031d47ad411ad4001d15b0a96aa23af7d026c4
mice85 <miguelangel.munoz@ua.es>
2020-11-24 11:51:14 +0100

    files moved to lib_calibration_cam2lid

--- online_calibration/scripts/code_backup/gpuProofs.m
+++
@@ -1,7 +0,0 @@
-function [a, b] = gpuProofs(kp_x, kp_y, pair_x, pair_y, tmp_x, tmp_y, escalar1, escalar2)
-
-a = (kp_x - pair_x) .* tmp_x * escalar1;
-b = (kp_y - pair_y) .* tmp_y * escalar2;
-
-end
-
