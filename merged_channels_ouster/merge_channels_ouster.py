1653aeb9607319569e6bca398073ac5ff7a25231
mice85 <munoz.banon@gmail.com>
2023-03-15 16:15:51 +0100

    Variable renamed.

--- merged_channels_ouster/merge_channels_ouster.py
+++ merged_channels_ouster/merge_channels_ouster.py
@@ -13,8 +13,8 @@ def callback(range_in, signal_in, reflec_in, nearir_in):
     global flag, bridge, merge_img_pub
     if flag:
         flag=False
-        #range = bridge.imgmsg_to_cv2(range_in)
-        #range = (range/256).astype('uint8')
+        depht = bridge.imgmsg_to_cv2(range_in)
+        depht = (depht/256).astype('uint8')
         signal = bridge.imgmsg_to_cv2(signal_in)
         signal =(signal/256).astype('uint8')
         reflec = bridge.imgmsg_to_cv2(reflec_in)
@@ -22,7 +22,8 @@ def callback(range_in, signal_in, reflec_in, nearir_in):
         nearir = bridge.imgmsg_to_cv2(nearir_in)
         nearir =(nearir/256).astype('uint8')
 
-        merged=cv2.merge([signal,nearir,reflec])
+        #merged=cv2.merge([signal,nearir,reflec])
+        merged=cv2.merge([nearir,reflec,depht])
         image_message = bridge.cv2_to_imgmsg(merged, "bgr8")  
         image_message.header.stamp = range_in.header.stamp
         image_message.header.frame_id = "ouster_sensor"
