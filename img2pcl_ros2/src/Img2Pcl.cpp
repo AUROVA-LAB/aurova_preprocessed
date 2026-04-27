1fa8a9ec1635590a53838db010f2f48322c2872e
Moisesraa275 <moisesdrg5@gmail.com>
2026-04-27 11:04:45 +0200

    Update Img2Pcl.cpp
    Cambio en una libreria al pasar a ROS2 Jazzy

--- img2pcl_ros2/src/Img2Pcl.cpp
+++ img2pcl_ros2/src/Img2Pcl.cpp
@@ -5,7 +5,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include <cv_bridge/cv_bridge.h>
+#include <cv_bridge/cv_bridge.hpp>
 #include <opencv2/opencv.hpp>
 
 #include <Eigen/Dense>
